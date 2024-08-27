f8ba4559e81365575d0fbd7876ebbab4a8580dfe
adamzales <127839305+adamzales@users.noreply.github.com>
2024-08-27 11:55:15 +0200

    Create Locales.lua

---
+++ az_beranidlo_esx/Locales.lua
@@ -0,0 +1,7 @@
+Locales = {}
+
+Locales.locpicking = 'Lockpicking'
+Locales.breaching = 'Breaching'
+Locales.hacking = 'Hacking'
+Locales.HackFailTit = 'Hack Failed'
+Locales.HackFailDesc = 'Hack has failed brach the door now'
